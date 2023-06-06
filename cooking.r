b1d9f1305eaf46e1d552910006af6c980645eb7b
jose luis cebrian dominguez <cebrian.pepe@gmail.com>
2023-06-06 18:56:06 +0200

    Update cooking.r

--- cooking.r
+++ cooking.r
@@ -1,4 +1,6 @@
-cooking <- function(AC, cooksd, PESO_SP, PUERTO2, ESPECIE, ESTRATO_RIM, sp3, MN, FECHA) {
+cooking <- function
+         (AC, cooksd, PESO_SP, PUERTO2, ESPECIE, ESTRATO_RIM, sp3, MN, FECHA)
+{
 temp_plot  <-ggplot(data =AC,
          mapping = aes(y = cooksd, x=TALLA_MEDIA_MAREA, col=factor(PUERTO2)))  +
     geom_quasirandom(aes(colour = PUERTO2, size=2,x=TALLA_MEDIA_MAREA, y=cooksd  ),
